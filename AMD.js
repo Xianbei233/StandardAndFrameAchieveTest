5d4f947e1752732e3d941efcb4cf2894d1a8b1a0
Xianbei233 <455482921@qq.com>
2018-12-17 22:41:44 +0800

    add dependency

--- AMD.js
+++ AMD.js
@@ -6,6 +6,12 @@
 let factories = {}
 
 function define(moduleName, dependencies, factory) {
+
+    factory.dependencies = dependencies
+    //将模块的依赖写入factory中,此处将factory当作一个实例
+    //write the dependencies of module into module's factory.
+    //The factory here is taken for a instance of class Function
+
     factories[moduleName] = factory
     //object[key]的写法等同于object.key,但是少了后一种写法的多种key命名限制
     //object[key] is equal to object.key but less limitation
@@ -13,12 +19,28 @@ function define(moduleName, dependencies, factory) {
 
 function require(modules, callback) {
     let result = modules.map(function (module) {
+
         //获取module数组内当前module的factory函数
         //get the current module's factory function in Array modules
         let factory = factories[module]
-        //获取factory函数的return结果
-        //get the current module's factory function return value
-        let exports = factory();
+
+        //获取module的依赖项
+        //get the dependencies of current required module                
+        let dependencies = factory.dependencies;
+
+        let exports;
+
+        //获取依赖模块的factory函数的return结果作为arguments
+        //get the return values of dependencies' factory as the callback function's arguments                   
+        require(dependencies, (...arguments) => {
+            exports = factory.apply(null, arguments)
+            //获取factory函数的return结果
+            //get the current module's factory function return value
+        })
+        //此处()=>{exports = factory.apply(null, arguments)}可替换成factory,但是无法将factory结果传出
+        //()=>{exports = factory.apply(null, arguments)} is equal to factory but can not pass the return value to exports
+
+
         //返回存入result中
         //put the return value into Array result
         return exports
@@ -36,6 +58,20 @@ define('two', [], () => {
     return 2
 })
 
-require(['one', 'two', (one, two) => {
-    console.log(one, two)
-}])
+require(['one', 'two'], (one, two) => {
+    console.log(one, two) //result is 1,2
+})
+
+//使用例(带依赖)
+
+define('one', [], () => {
+    return 1
+})
+
+define('two', ['one'], (one) => {
+    return 2 + one
+})
+
+require(['two'], (two) => {
+    console.log(two) //result is 3
+})
