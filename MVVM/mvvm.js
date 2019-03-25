320f70d83de0ece306b6e61238d81b425c32d2d9
Xianbei233 <455482921@qq.com>
2019-03-25 15:47:23 +0800

    奇码共赏，我修不动了，computed属性不知为何就是不能成功订阅

--- MVVM/mvvm.js
+++ MVVM/mvvm.js
@@ -5,7 +5,8 @@ function Vue(options = {}) {
     //将对象交给观察者进行obj.defineProprety
 
     //将this代理this._data
-    for (key in data) {
+
+    for (let key in data) {
 
         Object.defineProperty(this, key, {
             configurable: true,//默认false，无法删除
@@ -20,24 +21,27 @@ function Vue(options = {}) {
             set(newVal) {
                 //属性值设置，调用该方法
                 //此方法存在时，不可有writable属性
+
                 this._data[key] = newVal
             }
         })
     }
-    initComputed();
+
+
+    initComputed.call(this);
     new Compile(options.el, this)
 
 }
 
-function initComputed(){ //具有缓存功能
-    let vm = this;
-    let computed = this.$options.computed
-    Object.keys(computed).forEach((key)=>{
-        Object.defineProperty(vm,key,{
-            get(){
-                return typeof computed[key]==='function'?computed[key]:computed[key].get
+function initComputed() { //具有缓存功能
+    let vm = this
+    let computed = vm.$options.computed
+    Object.keys(computed).forEach((key) => {
+        Object.defineProperty(vm, key, {
+            get() {
+                return typeof computed[key] === 'function' ? computed[key] : computed[key].get
             },
-            set(){
+            set() {
 
             }
         })
@@ -60,6 +64,7 @@ function Observer(data) {
                 if (Dep.target) {
                     dep.addSub(Dep.target)
                 }
+                console.log(dep.subs)
                 return val
 
             },
@@ -71,12 +76,13 @@ function Observer(data) {
                 } else {
                     val = newVal
                     observer(newVal)
-                    Dep.notify()
+                    dep.notify()
                 }
 
 
             }
         })
+        observer(val)
     }
 }
 
@@ -89,37 +95,49 @@ function observer(data) {
 
 function Compile(el, vm) {
     //el 代表要编译的范围
+    console.log('渲染中')
     vm.$el = document.querySelector(el);
+
+
     let fragment = document.createDocumentFragment() //创建一个新的空白的文档片段（在内存中）
     //DocumentFragment 接口表示一个没有父级文件的最小文档对象。
     //它被当做一个轻量版的 Document 使用，用于存储已排好版的或尚未打理好格式的XML片段。
     //最大的区别是因为DocumentFragment不是真实DOM树的一部分，它的变化不会引起DOM树的重新渲染的操作(reflow) ，且不会导致性能等问题。
+
     while (child = vm.$el.firstChild) {
         fragment.appendChild(child)//此处操作会将真实dom节点转移进内存fragment中，会发生dom树结构变更
     }
+
     function replace(fragment) {
         Array.from(fragment.childNodes).forEach((node) => {
             let text = node.textContent
             let reg = /\{\{(.*)\}\}/
+            //此处处理文本节点
             if (node.nodeType === 3 && reg.test(text)) {
                 let arr = RegExp.$1.split('.')
                 let val = vm;
+                console.log(arr)
                 arr.forEach((key) => {
                     val = val[key]
+                    console.log(val)
                 })
                 new Watcher(vm, RegExp.$1, (newVal) => {
                     node.textContent = text.replace(/\{\{(.*)\}\}/, newVal)
                 })
-                node.textContent = text.replace(/\{\{(.*)\}\}/, val)
 
+                node.textContent = text.replace(/\{\{(.*)\}\}/, val)
+                console.log(node.textContent)
+                console.log('----------------------')
             }
+            //此处处理元素节点
             if (node.nodeType === 1) {
                 let nodeAttrs = node.attributes;
                 //类数组
                 Array.from(nodeAttrs).forEach(attr => {
                     let name = attr.name
                     let exp = attr.value
-                    if (name.indexOf('v-')) {
+                    let reg = /^v-/
+                    if (reg.test(name)) {
                         node.value = vm[exp]
 
                     }
@@ -132,18 +150,24 @@ function Compile(el, vm) {
                     })
                 })
             }
-            if (fragment.childNodes) {
+            if (node.childNodes) {
                 replace(node)
             }
         })
     }
 
-    vm.$el.appendChild(fragment)
+    setTimeout(function () {
+        replace(fragment)
+
+        vm.$el.appendChild(fragment)
+        console.log(vm.$el)
+    }, 1000)
+
 }
 
 //发布订阅
 function Dep() {
-    this, subs = []
+    this.subs = []
 }
 
 Dep.prototype.addSub = function (sub) {
